69f67166e22fd22e011c17f8562a3a6264587b98
felipepantoja1201 <53223898+felipepantoja1201@users.noreply.github.com>
2026-02-27 15:46:47 +0100

    Update sciat.js

--- scripts/sciat.js
+++ scripts/sciat.js
@@ -11,20 +11,20 @@ define([
     category : {
       name  : "FitnessOption",
       title : {
-        media : { word : "Fitness-oriented option" },
+        media : { word : "Fitness item" },
         css   : { "font-size" : "2em" },
         height: 7
       },
       media : [
-        { word : "protein bar" },
-        { word : "pre-workout" },
-        { word : "fat burner" },
-        { word : "performance supplement" },
-        { word : "creatine" },
-        { word : "whey protein" },
-        { word : "zero sugar energy drink" },
-        { word : "cutting pills" },
-        { word : "steroids" },
+        { word : "protein supplements" },
+        { word : "pre-workout powder" },
+        { word : "fat burner pills" },
+        { word : "metabolism booster" },
+        { word : "alcohol-free beer" },
+        { word : "meal replacement shake" },
+        { word : "zero sugar soda" },
+        { word : "intermittent fasting" },
+        { word : "hormonal modulation" },
         { word : "weight-loss injection" }
       ],
       css : { "font-size" : "3em" }
